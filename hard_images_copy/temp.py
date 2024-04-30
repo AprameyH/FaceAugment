42caf1e49e0b798478b372a99709bb34009cde13
tylerlee2020 <tyler.lee754@gmail.com>
2024-04-29 21:50:50 -0500

    Adding in results for fine-tuning: training on 2 source images of target + 6 source inages of others and testing on all the source images

--- hard_images_copy/temp.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(os.getcwd())
-path = "C:\\Users\\tyler\\OneDrive\\Documents\\FaceAugment\\hard_images_copy\\Yui_Aragaki"
-files = os.listdir(path)
-
-for index, filename in enumerate(files):
-    new_name = 'ya' + str(index) + '.jpg'
-    os.rename(os.path.join(path, filename), os.path.join(path, new_name))
